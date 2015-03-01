d51e40f5a1afde91a040aa185bb62186f8bd1544
Tomy <Tomy@192.168.11.8>
2015-03-01 23:25:19 +0900

    add bower components

---
+++ WebContent/components/app.js
@@ -0,0 +1,6 @@
+requirejs([ 'jquery', 'angular'], function($, angular) {
+	// jQuery, canvas and the app/sub module are all
+	// loaded and can be used here now.
+	
+	$("#hello").text("hello");
+});
