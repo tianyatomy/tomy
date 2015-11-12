6f09a1cb0d19013d9d34e92705249bbaa5cbf67e
Tomy <tianyatomy@gmail.com>
2015-11-13 00:31:16 +0900

    reset git repository

--- WebContent/components/app.js
+++
@@ -1,6 +0,0 @@
-define(function() {
-	// jQuery, canvas and the app/sub module are all
-	// loaded and can be used here now.
-	angular.module("app", ['coreModule']);
-	//$("#hello").text("hello");
-});
---
+++ tomy/WebContent/components/app.js
@@ -0,0 +1,6 @@
+define(function() {
+	// jQuery, canvas and the app/sub module are all
+	// loaded and can be used here now.
+	angular.module("app", ['coreModule']);
+	//$("#hello").text("hello");
+});
